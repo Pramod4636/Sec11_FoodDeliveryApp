ac42a9692c1953235b5aac03d37f9c8cbd4a095f
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-10 16:51:21 +0530

    Mealsummary make , AvailableMeallist just name printing , combined from Meal component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,15 +1,23 @@
 import React from 'react'
-
-
+import DUMMY_MEALS from "./DUMMY_MEAL"
+import classes from './AvailableMeals.module.css'
 
 
 
 const AvailableMeals = () => {
-  return (
-    <div>
-      
-    </div>
+   
+    const mealsList = DUMMY_MEALS.map( (meal) => {
+       return ( <li> {meal.name}</li> ); 
+    });
+
+
+    return (
+   <section className = {classes['meals']}>
+      <ul>
+         {mealsList}
+      </ul>
+   </section>
   )
-}
+};
 
 export default AvailableMeals
