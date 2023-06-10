60d266adefeaac94234912a90881df3c19a93c01
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-10 17:22:54 +0530

    MealItem is added and added in AvailbleMeals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,11 +3,11 @@ import DUMMY_MEALS from "./DUMMY_MEAL"
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 
-
+import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
    
     const mealsList = DUMMY_MEALS.map( (meal) => {
-       return ( <li> {meal.name}</li> ); 
+       return ( <MealItem  key={meal.id} name = {meal.name} description = {meal.description} price = {meal.price} />); 
     });
 
 
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import classes from './MealItem.module.css'
+const MealItem = (props) => {
 
-const MealItem = () => {
+  const price = `$${props.price.toFixed(2)}`;  
   return (
     <li>
-    <div>
-      
+    
+    <div className= {classes['meal']}>
+        <h3>{props.name}</h3>
+        <div className = {classes['summary']}>{props.description}</div>
+        <div className= {classes['price']}>{price}</div>   
+        <hr />
     </div>
+    
+    <div></div>
  
     </li>
  
