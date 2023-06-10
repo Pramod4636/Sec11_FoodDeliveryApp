0781e8df033f0f1f464d481fe077bf7f1dcc28d5
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-10 19:35:05 +0530

    logic is added for when click on bacgroud when close the  your cart which display using protal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Modal.module.css'
 import ReactDOM from 'react-dom' 
 const Backdrop = props => {
-   return <div className= {classes['backdrop'] }/>
+   return <div className= {classes['backdrop'] } onClick = {props.onClose}/>
      
 };
 
@@ -18,7 +18,7 @@ const Modal = (props) => {
   
     return (
      <React.Fragment>
-        {ReactDOM.createPortal(<Backdrop/> , portalElement)} 
+        {ReactDOM.createPortal(<Backdrop onClose = { props.onClose}/> , portalElement)} 
         {ReactDOM.createPortal(<ModalOverlay> {props.children} </ModalOverlay> , portalElement)}
      </React.Fragment>
   )
