bd85a0da717ac4947c2a3da1ecddfa39cc897c2b
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-17 14:45:45 +0530

    SEction 18 adding adding order form  ( validating ) submitting order to backend server (HTTP ) Fetching Meals Data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart'
 import CartProvider from './store/CartProvider';
 
-
+    
 function App() {
 
   const [cartIsShown,setCartIsShown] = useState(false);
