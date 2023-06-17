c0bebd915b55746b359bdfb5830b4115e37a560f
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-17 15:04:04 +0530

    inital rendering component using Useeffect send rquest to server and fetched meals data and meals state set with loaded data Note : we directly cann't use asynchronize function in useEffect . nested function defined and called

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,10 +3,41 @@ import DUMMY_MEALS from "./DUMMY_MEAL"
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 
+import { useEffect } from 'react';
+import {useState} from 'react'; 
+
 import MealItem from './MealItem/MealItem';
+
 const AvailableMeals = () => {
    
-    const mealsList = DUMMY_MEALS.map( (meal) => {
+   const [meals,setMeals] = useState([]);
+    useEffect( ()=> {
+    
+         const fetchMeals = async () => 
+         {
+            
+           const response = await fetch("https://react-http-264e0-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json") ;
+           const responseData = await response.json();
+
+          const loadedMeals = [];
+
+           for( const key in responseData ) { 
+               loadedMeals.push( {
+                  id : key ,
+                  name : responseData[key]["name"],
+                  description : responseData[key]["description"] ,
+                  price : responseData[key]["price"] 
+               });
+           }
+
+            setMeals(loadedMeals);
+         }
+  
+         fetchMeals();
+
+      }, []);
+
+    const mealsList = meals.map( (meal) => {
        return ( <MealItem   
           key={meal.id} 
           id = {meal.id}
