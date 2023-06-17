419b8a14fef644223f8c0134ae9f8284d8ceb64c
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-17 16:47:42 +0530

    add validation based on valid values show msg for each input if notValidValue and change the css dynamicall  Note : till now 1 promblem is 2 more elment in order select then button is not shown properly correct karache ahe

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -28,7 +28,7 @@ const Checkout = (props) => {
       const enteredCity = cityInputRef.current.value;
       
       const enteredNameIsValid = !isEmpty(enteredName);
-      const enteredStreetIsValid = !isEmpty(enteredStreetIsValid);
+      const enteredStreetIsValid = !isEmpty(enteredStreet);
       const enteredCityIsValid = !isEmpty(enteredCity);
       const enteredPostalCodeIsValid = !isFiveChars(enteredPostalCode); 
 
@@ -40,7 +40,7 @@ const Checkout = (props) => {
 
      }) ; 
 
-     
+
 
 
       const formIsValid = 
@@ -60,26 +60,38 @@ const Checkout = (props) => {
     const postalCodeInputRef = useRef() ; 
     const cityInputRef = useRef() ; 
     
+     const nameControlClasses = `${classes.control} ${formInputsValidity.name ? '' : classes.invalid }` ;
+     const streetControlClasses = `${classes.control} ${formInputsValidity.street ? '' : classes.invalid }` ;
+     const postCodeControlClasses = `${classes.control} ${formInputsValidity.postalCode ? '' : classes.invalid }` ;
+     
+     const cityControlClasses = `${classes.control} ${formInputsValidity.city ? '' : classes.invalid }` ;
+     
+
 
     return (
     <form className= {classes.form} onSubmit = {confirmHandler} >
-        <div className={classes.control}>
+        <div className= {nameControlClasses}>
             <label htmlFor="name"> Your Name </label>
             <input type="text" id="name" ref = {nameInputRef}/>
+            { !formInputsValidity.name && <p  >Please enter valid name! </p>}
         </div>
-        <div className={classes.control}>
+        <div className={streetControlClasses}>
             <label htmlFor="street"> Street </label>
             <input type="text" id="street" ref = {streetInputRef} />
+            { !formInputsValidity.street && <p >Please enter valid street! </p>}
         </div>
-        <div className={classes.control}>
+        <div className= {postCodeControlClasses} >
             <label htmlFor="postal"> Postal code  </label>
             <input type="text" id="postal" ref = {postalCodeInputRef} />
+            { !formInputsValidity.postalCode && <p >Please enter valid PostalCode ! </p>}
         </div>
-        <div className={classes.control}>
+        <div className = {cityControlClasses} >
             <label htmlFor="city"> City   </label>
             <input type="text" id="city" ref = {cityInputRef} />
+            { !formInputsValidity.city && <p  >Please enter valid City! </p>}
+        
         </div>
-        <button type = "button" onClick = {props.onCancel}>Cancel </button>
+        <button className = {classes.actions} type = "button" onClick = {props.onCancel}>Cancel </button>
         <button>Confirm </button>
         
     </form>    
